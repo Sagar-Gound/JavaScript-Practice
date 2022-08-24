9934a82dc844400c774459d54823012de936a341
Sagar Gound <sagargound0410@gmail.com>
2022-08-24 14:44:59 +0530

    completed the typing speed calculator project

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const setOfWords=[
     "My name is Sagar Subhashchandra Gound and I am a Developer.",
     "Hope you are having fun, this is a simple game that you can make.",
-    "If youn want to learn to code then, follow YouTube channels for the same and along with the tutorial start practicing.",
+    "If you want to learn to code then, follow YouTube channels for the same and along with the tutorial start practicing.",
 ];
 
 const msg = document.getElementById('msg');
 const typeWords = document.getElementById('myWords');
 const btn = document.getElementById('btn');
 let startTime, endTime;
+
 const playGame = () =>{
     let randomNumber = Math.floor(Math.random()*setOfWords.length);
     msg.innerText = setOfWords[randomNumber];
@@ -20,17 +21,45 @@ const endGame = () =>{
     let date = new Date();
     endTime = date.getTime();
     let totalTime = ((endTime-startTime)/1000);
+    let totalStr = typeWords.value;
+    let wordCount = wordCounter(totalStr);
+    let speed = Math.round((wordCount / totalTime)*60);
+    let finalMsg = "You typed total at "+ speed +" words per minutes. "
+    // console.log(finalMsg);
+    finalMsg += compareWords(msg.innerText, totalStr);
+    msg.innerText = finalMsg;
+};
 
-    let totalStr = 
+const compareWords = (str1,str2)=>{
+    let words1 = str1.split(" ");
+    let words2 = str2.split(" ");
+    let cnt = 0;
+    words1.forEach(function (item, index) {
+        if(item == words2[index]){
+            cnt++;
+        }
+    })
+
+    let errorWords = (words1.length-cnt);
+    return (cnt + " correct out of "+words1.length+" words and the total number of error are "+errorWords+".");
+
+};
+
+const wordCounter = (str) => {
+    let response = str.split(" ").length
+    return response;
 };
 
 btn.addEventListener('click', function(){
         if(this.innerText == 'Start'){
-        typeWords.disabled == false;
-        playGame();
-    }else if(this.innerText == "Done"){
-        typeWords.disabled = true;
-        btn.innerText = "Start";
-     endGame();
-    }
-})
+            typeWords.disabled = false;
+            typeWords.value = "";
+            playGame();
+            typeWords.focus() = true;
+        }else if(this.innerText == "Done"){
+            typeWords.disabled = true;
+            endGame();
+            btn.innerText = "Start";
+            typeWords.innerText = "";
+        }
+});
